6312d8703d24486045f5b3812abd2b377ce63c09
kareem444 <kareem963221@gmail.com>
2021-11-26 23:35:06 +0200

    add web socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,25 +9,41 @@ import { ChatsModule } from './chats/chats.module';
 import { MessagesModule } from './messages/messages.module';
 import { FriendsModule } from './friends/friends.module';
 import { FriendRequestsModule } from './friend-requests/friend-requests.module';
+import { EventsModule } from './events/events.module';
+import { EventsGateway } from './events/events.gateway';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'ec2-34-194-25-190.compute-1.amazonaws.com',
+    //   port: 5432,
+    //   username: 'efgsbvgbyxgfgw',
+    //   password: '3558220b5a606da29d88e00ae76b79457f80f41e8fa84ff80f4a179d50688c3d',
+    //   database: 'da5rgloqe3ka7n',
+    //   ssl: {
+    //     rejectUnauthorized: false,
+    //   },
+    //   entities: ["dist/**/*.entity{.ts,.js}"],
+    //   synchronize: true,
+    // }),
+    // TypeOrmModule.forRoot({
+    //   // type: 'postgres',
+    //   // host: 'localhost',
+    //   // port: 5432,
+    //   // username: 'postgres',
+    //   // password: 'kareem44',
+    //   // database: 'chat_server',
+    //   // entities: ["dist/**/*.entity{.ts,.js}"],
+    //   // synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
-      // type: 'postgres',
-      // host: 'localhost',
-      // port: 5432,
-      // username: 'postgres',
-      // password: 'kareem44',
-      // database: 'chat_server',
-      type: 'postgres',
-      host: 'ec2-34-194-25-190.compute-1.amazonaws.com',
-      port: 5432,
-      username: 'efgsbvgbyxgfgw',
-      password: '3558220b5a606da29d88e00ae76b79457f80f41e8fa84ff80f4a179d50688c3d',
-      database: 'da5rgloqe3ka7n',
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'chat',
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true,
     }),
@@ -38,6 +54,7 @@ import { FriendRequestsModule } from './friend-requests/friend-requests.module';
     ConfigModule.forRoot(),
     FriendsModule,
     FriendRequestsModule,
+    EventsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/events/events.gateway.ts
@@ -0,0 +1,34 @@
+import {
+    SubscribeMessage,
+    WebSocketGateway,
+    OnGatewayInit,
+    WebSocketServer,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ namespace: 'socket' })
+export class EventsGateway
+    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer()
+    server: Server;
+
+    @SubscribeMessage('message')
+    findMessage(client: Socket, message: any) {
+        this.server.emit('message', message);
+        console.log(message);
+    }
+
+    afterInit(server: Server) {
+        console.log('init');
+    }
+
+    handleConnection(client: Socket, ...args: any[]) {
+        console.log(`Client connected: ${client.id}`);
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log(`Client disconnected: ${client.id}`);
+    }
+}
