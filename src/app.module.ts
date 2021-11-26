e5a7faff5caf38621d78740627ca17d689d426f6
kareem444 <kareem963221@gmail.com>
2021-11-26 23:38:14 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,19 +14,19 @@ import { EventsGateway } from './events/events.gateway';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'ec2-34-194-25-190.compute-1.amazonaws.com',
-    //   port: 5432,
-    //   username: 'efgsbvgbyxgfgw',
-    //   password: '3558220b5a606da29d88e00ae76b79457f80f41e8fa84ff80f4a179d50688c3d',
-    //   database: 'da5rgloqe3ka7n',
-    //   ssl: {
-    //     rejectUnauthorized: false,
-    //   },
-    //   entities: ["dist/**/*.entity{.ts,.js}"],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-34-194-25-190.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'efgsbvgbyxgfgw',
+      password: '3558220b5a606da29d88e00ae76b79457f80f41e8fa84ff80f4a179d50688c3d',
+      database: 'da5rgloqe3ka7n',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
     // TypeOrmModule.forRoot({
     //   // type: 'postgres',
     //   // host: 'localhost',
@@ -37,16 +37,16 @@ import { EventsGateway } from './events/events.gateway';
     //   // entities: ["dist/**/*.entity{.ts,.js}"],
     //   // synchronize: true,
     // }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'chat',
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'chat',
+    //   entities: ["dist/**/*.entity{.ts,.js}"],
+    //   synchronize: true,
+    // }),
     UsersModule,
     AuthModule,
     ChatsModule,
