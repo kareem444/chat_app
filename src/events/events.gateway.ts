558c0a3a57eb0262140ec12cec70273f4a923ff6
kareem444 <kareem963221@gmail.com>
2021-11-27 20:15:32 +0200

    change the socket client

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -14,10 +14,25 @@ export class EventsGateway
     @WebSocketServer()
     server: Server;
 
+    @SubscribeMessage('join')
+    joinToRoom(client: Socket, room: any) {
+        client.join(room.toString());
+    }
+
     @SubscribeMessage('message')
     findMessage(client: Socket, message: any) {
-        this.server.emit('message', message);
-        console.log(message);
+        this.server.to(message['chat']).emit('message', message);
+    }
+
+    @SubscribeMessage('typing')
+    typingMessage(client: Socket, typing: any) {
+        this.server.emit('typing', typing);
+    }
+
+    @SubscribeMessage('friend_request')
+    sendFriendRequest(client: Socket, data: any) {
+        this.server.to(data[0]).emit('friend_request', data[1]);
+        console.log(data);
     }
 
     afterInit(server: Server) {
